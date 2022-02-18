59255dd6b14bcead8a469b02f5aee94b67f446d3
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-17 21:06:12 -0500

    creating tables

--- data/migrations/20220217223006_create_tables.js
+++ data/migrations/20220217223006_create_tables.js
@@ -1,8 +1,25 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable().unique();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredients_id");
+      tbl.string("ingredient_name", 128).notNullable().unique();
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments();
+    })
+    .createTable("step_ingredients", (tbl) => {
+      tbl.increments();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists("step_ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };

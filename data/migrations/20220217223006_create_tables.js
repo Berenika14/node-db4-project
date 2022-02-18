dfa8da0195215898ecb1d58b5e29b12e4033c2e3
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-17 21:18:54 -0500

    created all the tables

--- data/migrations/20220217223006_create_tables.js
+++ data/migrations/20220217223006_create_tables.js
@@ -7,12 +7,40 @@ exports.up = async function (knex) {
     .createTable("ingredients", (tbl) => {
       tbl.increments("ingredients_id");
       tbl.string("ingredient_name", 128).notNullable().unique();
+      tbl.string("ingredient_unit", 50);
     })
     .createTable("steps", (tbl) => {
-      tbl.increments();
+      tbl.increments("step_id");
+      tbl.string("step_text", 128).notNullable();
+      tbl.string("step_number").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipes_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
     .createTable("step_ingredients", (tbl) => {
-      tbl.increments();
+      tbl.increments("step_ingredient_id");
+      tbl.float("quantity").notNullable();
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
